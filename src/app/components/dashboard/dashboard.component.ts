8bfbe180d435936da97de82a26a1bcc5ad4b707f
RiohRowe1 <riohrowe@gmail.com>
2019-10-11 04:40:25 -0500

    Merging Angular Projects on different branches.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -15,6 +15,11 @@ export class DashboardComponent implements OnInit
   spotifyAPIResponse:object;
   spotifyJsonResponse:any;
   apiExtension:HTMLInputElement;
+
+  obj:string;
+  jsonObj:JSON;
+  playlist_names:string[] = [];
+  track_names:string[] = [];
   
   constructor(private loginService:LoginService, private utilService:UtilService, private activatedRoute:ActivatedRoute, private router:Router) { }
 
@@ -36,6 +41,13 @@ export class DashboardComponent implements OnInit
           this.loginService.expireTime = responseJson.expires_in;
           this.loginService.refreshToken = responseJson.refresh_token;
           this.router.navigate(['/dashboard']);
+          // this.loadPlaylistArray();
+          // this.loadTrackArray();
+          this.askSpotifyLoadPlaylistArray();
+          this.askSpotifyLoadTrackArray();
+          console.log(window.location.href);
+          console.log("URL MF PARAMS: " + this.getUrlElements("code"));
+          
         }, error => 
         {
           console.log(error.error);
@@ -62,26 +74,72 @@ export class DashboardComponent implements OnInit
     }
   }
 
+  public getUrlElements(target:string) 
+  {
+    var urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(target);
+  }
+
+  loadPlaylistArrayhandler = (test) =>
+  {
+    console.log("from the playlist json obj:");
+    for(let i = 0; i < test.items.length; i++) {
+      console.log(test.items[i].name+ " " + test.items[i].tracks.href );
+      let tmp = test.items[i].name;
+      this.playlist_names.push(tmp);
+    }
+    console.log("from the playlist array:");
+    for(let i in this.playlist_names) {
+      console.log(test.items[i].name);
+    }
+  }
+
+  askSpotifyLoadPlaylistArray():void
+  { 
+    this.askSpotify(this.loadPlaylistArrayhandler, "/v1/me/playlists", "GET", null);
+  }
+
+  loadTrackArrayhandler = (song):void =>
+  {
+    console.log("from the tracks json obj:");
+    for(let i = 0; i < song.limit; i++) {
+      console.log(song.items[i].track.name);
+      let tmp = song.items[i].track.name;
+      this.track_names.push(tmp);
+    }
+
+    console.log("from the tracks array:");
   
+    for(let i in this.track_names) {
+       console.log("TRACK NAME: "+this.track_names[i]);
+     }
+  }
+
+  askSpotifyLoadTrackArray():void
+  { 
+    this.askSpotify(this.loadPlaylistArrayhandler, "/v1/playlists/54TJJfZ0s48JmS5ZIUN9T5/tracks", "GET", null);
+  }
+
+
  
  
   // Function that is run on the returned object when askSpotify Hits an endpoint.
   // This function must be defined using arrow notation to avoid referencing the
   // Wrong "this" while setting your variables.
-  handleRequestObject = (responseJson:object):void =>
-  {
-    this.spotifyJsonResponse = responseJson;
-    let element = document.getElementById("jsonResponseContainer");
-    element.innerHTML = "";
-    this.utilService.DisplayJson(this.spotifyJsonResponse, element, -1);
-  }
-  // A simple function that links a button press to an API endpoint Call with
-  // a particular requestObjectHandler.
-  askSpotify1():void
-  {
-    this.apiExtension = <HTMLInputElement>document.getElementById('dashboardAPIExtension'); 
-    this.askSpotify(this.handleRequestObject, this.apiExtension.value, "GET", null);
-  }
+  // handleRequestObject = (responseJson:object):void =>
+  // {
+  //   this.spotifyJsonResponse = responseJson;
+  //   let element = document.getElementById("jsonResponseContainer");
+  //   element.innerHTML = "";
+  //   this.utilService.DisplayJson(this.spotifyJsonResponse, element, -1);
+  // }
+  // // A simple function that links a button press to an API endpoint Call with
+  // // a particular requestObjectHandler.
+  // askSpotify1():void
+  // {
+  //   this.apiExtension = <HTMLInputElement>document.getElementById('dashboardAPIExtension'); 
+  //   this.askSpotify(this.handleRequestObject, this.apiExtension.value, "GET", null);
+  // }
 
 
   // Generalized API Endpoint function that takes a function of the form:
